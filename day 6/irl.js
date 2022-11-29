c04123587e2d44f8bde58f81631fbacf56dcfdbc
ubaidbeast <ubaidadineymar11.com>
2022-11-29 12:36:47 +0300

    added day 6 containing examples of add event listerner

---
+++ day 6/irl.js
@@ -0,0 +1,20 @@
+document.querySelector('#DELL').addEventListener('click', alertUersD)
+document.querySelector('#HP').addEventListener('click', alertUersH)
+document.querySelector('#MAC').addEventListener('click', alertUersM)
+document.querySelector('#WINDOWS').addEventListener('click', alertUersW)
+
+function alertUersD() {
+    alert('you clicked dell')
+}
+
+function alertUersH() {
+    alert('you clicked HP')
+}
+
+function alertUersM() {
+    alert('you clicked Mac')
+}
+
+function alertUersW() {
+    alert('you clicked Windows')
+}
